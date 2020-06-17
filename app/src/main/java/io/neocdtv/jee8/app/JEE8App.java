485088d3a2539617c2ca6b5c7dcf2f6921e0db0e
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-06-17 12:51:41 +0200

    add app

--- app/src/main/java/io/neocdtv/jee8/app/JEE8App.java
+++ app/src/main/java/io/neocdtv/jee8/app/JEE8App.java
@@ -1,7 +1,18 @@
 package io.neocdtv.jee8.app;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/")
-public class JEE8App {
+public class JEE8App extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    final Set<Class<?>> classes = new HashSet<>();
+    // register root resource
+    classes.add(StatusResource.class);
+    return classes;
+  }
 }
