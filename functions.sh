0b63089efaa6d54e33669fd13935a98eeb130911
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-08-16 00:28:40 -0700

    add download script for payara

--- functions.sh
+++ functions.sh
@@ -1,8 +1,16 @@
 source environment.sh
 CRIU_IMAGE_DIR="/tmp/payaraMicroJEE8ExampleImage"
+PAYARA_VERSION="5.2020.3";
 PAYARA_MICRO_ROOT_DIR="/tmp/payaraMicroJEE8Example"
+TMP_DIR="/var/tmp"
+PAYARA_APP="$TMP_DIR/payara-micro-$PAYARA_VERSION.jar"
 
 build_app() {
+  mvn clean install -f app/
+  echo "" > app/target/app/.reload
+}
+
+build_database() {
   mvn clean install -f app/
   echo "" > app/target/app/.reload
 }
@@ -33,7 +41,7 @@ run_app_server() {
 			    -Xverify:none\
 			    -Xdebug\
 			    -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address="$APP_DEBUG_PORT"\
-			    -jar /var/tmp/payara-micro.jar \
+			    -jar "$PAYARA_APP" \
 			    --deploy app/target/app\
 			    --nocluster \
 			    --contextroot / \
@@ -59,20 +67,30 @@ clean_up() {
   sudo rm -fr "$PAYARA_MICRO_ROOT_DIR"
 }
 
+get_app_server() {
+	echo "PAYARA::GET"
+	# check for latests payara version (https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/maven-metadata.xml metadata/versioning/latest or release?) and inform if differ from $PAYARA_VERSION
+	if [ ! -f "$PAYARA_APP" ]
+		then
+			curl "https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/$PAYARA_VERSION/payara-micro-$PAYARA_VERSION.jar" -o "$PAYARA_APP"
+	fi
+}
+
 is_app_running() {
   # TODO: pool, curl on status url
   sleep 18
 }
 
 # If you include this function in a another shell script and try using with criu it will fail. This has something todo with the fact that the script opens a new session (needs verification)
-# To be able to use criu use this function directly after source ./functions.sh
+# To be able to use criu use this function directly after source functions.sh
 run_app() {
   kill_app_server
   if [ -d "$CRIU_IMAGE_DIR" ]
 	then
 		criu_restore
 	else
-    run_app_server
+		get_app_server
+		run_app_server
 		is_app_running
 		criu_dump
 		criu_restore
