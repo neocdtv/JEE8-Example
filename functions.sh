233fa455fc32d1cd73f6df7b2ff1e7f34959506e
neocdtv <krzysztof.wolf@nttdata.com>
2020-08-19 18:30:45 +0200

    clean up

--- functions.sh
+++ functions.sh
@@ -18,23 +18,37 @@ docker_build_database() {
 }
 
 docker_run_database() {
-  docker-compose up database -d
+  docker-compose up -d database
   is_db_ready
   mvn flyway:migrate -f database/
 }
 
-docker_clean() {
-  echo "DOCKER::CLEAN"
-  docker kill jee8-example_app_1
+db_clean() {
+  echo "DATABASE::CLEAN"
   docker kill jee8-example_database_1
-  docker rm -f jee8-example_app_1
   docker rm -f jee8-example_database_1
-  docker rmi -f example/app
   docker rmi -f example/db
 }
 
-build_app() {
-  mvn clean install -f app/
+app_clean() {
+  echo "APP::CLEAN"
+  docker kill jee8-example_app_1
+  docker rm -f jee8-example_app_1
+  docker rmi -f example/app
+  kill_app_server
+}
+
+app_build() {
+  mvn clean -f app/
+  rebuild_app
+}
+
+app_rebuild() {
+  mvn prepare-package war:exploded -Dmaven.test.skip -f app/
+  redeploy_app
+}
+
+app_redeploy() {
   echo "" > app/target/app/.reload
 }
 
@@ -42,20 +56,15 @@ build_database() {
   mvn clean install -f database/
 }
 
-build_app_minimal() {
-	mvn prepare-package war:exploded -Dmaven.test.skip -f app/
-	echo "" > app/target/app/.reload
-}
-
 
 kill_app_server() {
-  echo "PAYARA::KILL"
-  PAYARA_MICRO_PROCESS_ID=`ps -au | grep java | grep payara-micro | awk '{print $2}'`
+  local prefix="PAYARA::KILL"
+  PAYARA_MICRO_PROCESS_ID=`ps -aux | grep java | grep payara-micro | awk '{print $2}'`
 	if [[ ! -z "$PAYARA_MICRO_PROCESS_ID" ]]; then
-		echo "Killing payara micro with pid '$PAYARA_MICRO_PROCESS_ID'";
+		echo "$prefix - Killing payara micro with pid '$PAYARA_MICRO_PROCESS_ID'";
 		kill -SIGKILL $PAYARA_MICRO_PROCESS_ID;
 	else
-		echo "Can't find running payara micro process!"
+		echo "$prefix - Can't find running payara micro process!"
 	fi
 }
 
@@ -76,18 +85,28 @@ run_app_server() {
 			    --rootDir "$PAYARA_MICRO_ROOT_DIR" &
 }
 
-criu_dump() {
-  echo "CRIU::DUMP"
+app_dump() {
+  echo "APP::DUMP"
 	rm -rf "$CRIU_IMAGE_DIR"
 	mkdir -p "$CRIU_IMAGE_DIR"
-	PAYARA_MICRO_PROCESS_ID=`ps -au | grep java | grep payara-micro | awk '{print $2}'`
-	sudo criu dump -vvvv -t $PAYARA_MICRO_PROCESS_ID --shell-job --log-file criu-dump.log --tcp-established --images-dir "$CRIU_IMAGE_DIR"
+	PAYARA_MICRO_PROCESS_ID=`ps -aux | grep java | grep payara-micro | awk '{print $2}'`
+	criu_dump
+}
+
+app_restore() {
+  echo "APP::RESTORE"
+  criu_restore
+	is_app_ready
+}
+
+criu_dump() {
+  echo "CRIU::DUMP"
+  sudo criu dump -vvvv --shell-job -t $PAYARA_MICRO_PROCESS_ID --log-file criu-dump.log --tcp-established --images-dir "$CRIU_IMAGE_DIR"
 }
 
 criu_restore() {
   echo "CRIU::RESTORE"
-	sudo criu-ns restore -vvvv --shell-job --log-file criu-restore.log --tcp-established --images-dir "$CRIU_IMAGE_DIR" &
-	is_app_ready
+  sudo criu-ns restore -vvvv --shell-job --log-file criu-restore.log --tcp-established --images-dir "$CRIU_IMAGE_DIR" &
 }
 
 clean_up() {
@@ -109,20 +128,23 @@ is_app_ready() {
 }
 
 check_response_code() {
+  local prefix="CHECK::APP"
   URL=http://$APP_HOST:$APP_HTTP_PORT/$1
   NEXT_WAIT_TIME=1
   MAX_WAIT_TIME=60
   SLEEP_TIME=0.25 # 250ms
   until [ $NEXT_WAIT_TIME -gt $MAX_WAIT_TIME ] || [ $(curl -s -o /dev/null -w "%{http_code}" $URL) == $2 ]; do
-    echo "CHECK::APP - Waiting for application to be ready to handle http traffic. Check url is $URL. Check $NEXT_WAIT_TIME of $MAX_WAIT_TIME."
+    echo "$prefix - Waiting for application to be ready to handle http traffic. Check url is $URL. Check $NEXT_WAIT_TIME of $MAX_WAIT_TIME."
     sleep $SLEEP_TIME
     (( NEXT_WAIT_TIME++ ))
   done
   if [ $NEXT_WAIT_TIME -gt $MAX_WAIT_TIME ]
   then
-    echo "CHECK::APP - Application is not ready to handle http traffic. Waited $MAX_WAIT_TIME x $SLEEP_TIME sec. Check url was $URL"
+    echo "$prefix - Application is not ready to handle http traffic. Waited $MAX_WAIT_TIME x $SLEEP_TIME sec. Check url was $URL"
+    return 1;
   else
-    echo "CHECK::APP - Application is ready to handle http traffic. Check url was $URL"
+    local waited=$((NEXT_WAIT_TIME-1))
+    echo "$prefix - Application is ready to handle http traffic. Waited $waited x $SLEEP_TIME sec. Check url was $URL"
   fi
 }
 
@@ -137,12 +159,12 @@ run_app() {
   kill_app_server
   if [ -d "$CRIU_IMAGE_DIR" ]
 	then
-		criu_restore
+		app_restore
 	else
 		get_app_server
 		run_app_server
 		is_app_ready
-		criu_dump
-		criu_restore
+		app_dump
+		app_restore
   fi
 }
