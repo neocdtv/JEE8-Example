ffb7c1147d1cbbf70537e21b81c1bbd78c0178b7
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-08-24 01:44:40 -0700

    small update

--- functions.sh
+++ functions.sh
@@ -1,75 +1,79 @@
 source environment.sh
-CRIU_IMAGE_DIR="/tmp/payaraMicroJEE8ExampleImage"
-PAYARA_VERSION="5.2020.3";
-PAYARA_MICRO_ROOT_DIR="/tmp/payaraMicroJEE8Example"
 TMP_DIR="/var/tmp"
+CRIU_IMAGE_DIR="$TMP_DIR/payaraMicroJEE8ExampleImage"
+PAYARA_MICRO_ROOT_DIR="$TMP_DIR/payaraMicroJEE8Example"
+PAYARA_VERSION="5.2020.3";
 PAYARA_APP="$TMP_DIR/payara-micro-$PAYARA_VERSION.jar"
 
-docker_build() {
-  docker-compose build
-}
-
-docker_build_app() {
-  docker-compose build app
-}
-
-docker_build_database() {
-  docker-compose build database
-}
-
-docker_run_database() {
+database_build() {
+  database_clean
+  print_info "DATABASE::BUILD"
   docker-compose up -d database
   is_db_ready
-  mvn flyway:migrate -f database/
+  mvn  flyway:migrate -f database/
 }
 
-db_clean() {
-  echo "DATABASE::CLEAN"
+database_run() {
+  print_info "DATABASE::RUN"
+  docker-compose up -d database
+  is_db_ready
+  mvn prepare-package flyway:migrate -f database/
+}
+
+database_clean() {
+  print_info "DATABASE::CLEAN"
   docker kill jee8-example_database_1
   docker rm -f jee8-example_database_1
   docker rmi -f example/db
+  mvn clean -f database/
 }
 
 app_clean() {
-  echo "APP::CLEAN"
+  print_info "APP::CLEAN"
   docker kill jee8-example_app_1
   docker rm -f jee8-example_app_1
   docker rmi -f example/app
-  kill_app_server
+  payara_kill
+  mvn clean -f app/
 }
 
 app_build() {
+  print_info "APP::BUILD"
   mvn clean -f app/
-  rebuild_app
+  app_rebuild
 }
 
 app_rebuild() {
+  print_info "APP::REBUILD"
   mvn prepare-package war:exploded -Dmaven.test.skip -f app/
-  redeploy_app
+  app_redeploy
 }
 
 app_redeploy() {
+  print_info "APP::REDEPLOY"
+  # get status timestamp
   echo "" > app/target/app/.reload
+  # wait for status timestamp to change
 }
 
-build_database() {
-  mvn clean install -f database/
-}
-
-
-kill_app_server() {
+payara_kill() {
   local prefix="PAYARA::KILL"
-  PAYARA_MICRO_PROCESS_ID=`ps -aux | grep java | grep payara-micro | awk '{print $2}'`
-	if [[ ! -z "$PAYARA_MICRO_PROCESS_ID" ]]; then
-		echo "$prefix - Killing payara micro with pid '$PAYARA_MICRO_PROCESS_ID'";
-		kill -SIGKILL $PAYARA_MICRO_PROCESS_ID;
+  local payara_pid="$(payara_find)"
+	if [[ ! -z "$payara_pid" ]]; then
+		print_info "$prefix - Killing payara micro with pid '$payara_pid'";
+		kill -SIGKILL $payara_pid;
 	else
-		echo "$prefix - Can't find running payara micro process!"
+		print_warn "$prefix - Can't find running payara micro process!"
 	fi
 }
 
-run_app_server() {
-  echo "PAYARA::RUN"
+payara_find() {
+  local pid=`ps -aux | grep java | grep payara-micro | awk '{print $2}'`
+  echo "$pid"
+}
+
+payara_run() {
+  print_info "PAYARA::RUN"
   java    -XX:-UsePerfData\
 			    -XX:+TieredCompilation\
 			    -XX:TieredStopAtLevel=1\
@@ -86,26 +90,26 @@ run_app_server() {
 }
 
 app_dump() {
-  echo "APP::DUMP"
-	rm -rf "$CRIU_IMAGE_DIR"
-	mkdir -p "$CRIU_IMAGE_DIR"
-	PAYARA_MICRO_PROCESS_ID=`ps -aux | grep java | grep payara-micro | awk '{print $2}'`
-	criu_dump
+  print_info "APP::DUMP"
+  rm -rf "$CRIU_IMAGE_DIR"
+  mkdir -p "$CRIU_IMAGE_DIR"
+  criu_dump
 }
 
 app_restore() {
-  echo "APP::RESTORE"
+  print_info "APP::RESTORE"
   criu_restore
-	is_app_ready
+  is_app_ready
 }
 
 criu_dump() {
-  echo "CRIU::DUMP"
-  sudo criu dump -vvvv --shell-job -t $PAYARA_MICRO_PROCESS_ID --log-file criu-dump.log --tcp-established --images-dir "$CRIU_IMAGE_DIR"
+  print_info "CRIU::DUMP"
+  local payara_pid="$(payara_find)"
+  sudo criu dump -vvvv --shell-job -t $payara_pid --log-file criu-dump.log --tcp-established --images-dir "$CRIU_IMAGE_DIR"
 }
 
 criu_restore() {
-  echo "CRIU::RESTORE"
+  print_info "CRIU::RESTORE"
   sudo criu-ns restore -vvvv --shell-job --log-file criu-restore.log --tcp-established --images-dir "$CRIU_IMAGE_DIR" &
 }
 
@@ -114,13 +118,13 @@ clean_up() {
   sudo rm -fr "$PAYARA_MICRO_ROOT_DIR"
 }
 
-get_app_server() {
-	echo "PAYARA::GET"
-	# check for latests payara version (https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/maven-metadata.xml metadata/versioning/latest or release?) and inform if differ from $PAYARA_VERSION
-	if [ ! -f "$PAYARA_APP" ]
-		then
-			curl "https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/$PAYARA_VERSION/payara-micro-$PAYARA_VERSION.jar" -o "$PAYARA_APP"
-	fi
+payara_download() {
+  print_info "PAYARA::DOWNLOAD"
+  # check for latests payara version (https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/maven-metadata.xml metadata/versioning/latest or release?) and inform if differ from $PAYARA_VERSION
+  if [ ! -f "$PAYARA_APP" ]
+  then
+    curl "https://repo1.maven.org/maven2/fish/payara/extras/payara-micro/$PAYARA_VERSION/payara-micro-$PAYARA_VERSION.jar" -o "$PAYARA_APP"
+  fi
 }
 
 is_app_ready() {
@@ -134,37 +138,61 @@ check_response_code() {
   MAX_WAIT_TIME=60
   SLEEP_TIME=0.25 # 250ms
   until [ $NEXT_WAIT_TIME -gt $MAX_WAIT_TIME ] || [ $(curl -s -o /dev/null -w "%{http_code}" $URL) == $2 ]; do
-    echo "$prefix - Waiting for application to be ready to handle http traffic. Check url is $URL. Check $NEXT_WAIT_TIME of $MAX_WAIT_TIME."
+    print_info "$prefix - Waiting for application to handle http traffic. Check url is $URL. Check $NEXT_WAIT_TIME of $MAX_WAIT_TIME."
     sleep $SLEEP_TIME
     (( NEXT_WAIT_TIME++ ))
   done
   if [ $NEXT_WAIT_TIME -gt $MAX_WAIT_TIME ]
   then
-    echo "$prefix - Application is not ready to handle http traffic. Waited $MAX_WAIT_TIME x $SLEEP_TIME sec. Check url was $URL"
+    print_error "$prefix - Application is not ready to handle http traffic. Waited $MAX_WAIT_TIME x $SLEEP_TIME sec. Check url was $URL"
     return 1;
   else
     local waited=$((NEXT_WAIT_TIME-1))
-    echo "$prefix - Application is ready to handle http traffic. Waited $waited x $SLEEP_TIME sec. Check url was $URL"
+    print_info "$prefix - Application is ready to handle http traffic. Waited $waited x $SLEEP_TIME sec. Check url was $URL"
   fi
 }
 
 is_db_ready() {
   # TODO:
-  sleep 10
+  sleep 5
 }
 
 # If you include this function in a another shell script and try using with criu it will fail. This has something todo with the fact that the script opens a new session (needs verification)
 # To be able to use criu use this function directly after source functions.sh
-run_app() {
-  kill_app_server
+app_run() {
+  payara_kill
   if [ -d "$CRIU_IMAGE_DIR" ]
 	then
 		app_restore
 	else
-		get_app_server
-		run_app_server
-		is_app_ready
+		payara_download
+		payara_run
+		is_app_ready # check return code and return if !=0
 		app_dump
 		app_restore
   fi
+}
+
+COLOR_GREEN='\033[1;34m'
+COLOR_BROWN='\033[0;33m'#  
+COLOR_RED='\033[0;31m'
+FORMAT_BOLD='\033[1m'
+FORMAT_RESET='\033[0m'
+
+print_info() {
+  echo -e "${COLOR_GREEN}[INFO]${FORMAT_RESET} ------------------------------------------------------------------------"
+  echo -e "${COLOR_GREEN}[INFO]${FORMAT_RESET}${FORMAT_BOLD} $1 ${FORMAT_RESET}"
+  echo -e "${COLOR_GREEN}[INFO]${FORMAT_RESET} ------------------------------------------------------------------------"
+}
+
+print_warn() {
+  echo -e "${COLOR_BROWN}[WARNING]${FORMAT_RESET} ------------------------------------------------------------------------"
+  echo -e "${COLOR_BROWN}[WARNING]${FORMAT_RESET}${FORMAT_BOLD} $1 ${FORMAT_RESET}"
+  echo -e "${COLOR_BROWN}[WARNING]${FORMAT_RESET} ------------------------------------------------------------------------"
+}
+
+print_error() {
+  echo -e "${COLOR_RED}[ERROR]${FORMAT_RESET} ------------------------------------------------------------------------"
+  echo -e "${COLOR_RED}[ERROR]${FORMAT_RESET}${FORMAT_BOLD} $1 ${FORMAT_RESET}"
+  echo -e "${COLOR_RED}[ERROR]${FORMAT_RESET} ------------------------------------------------------------------------"
 }
