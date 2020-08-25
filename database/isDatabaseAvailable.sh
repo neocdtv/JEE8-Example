8ad53a6656d3b34f1bf86a90c8b8cee8fb6f35ae
neocdtv <krzysztof.wolf.1980@gmail.com>
2020-08-25 09:36:17 -0700

    remove unused scirpt

--- database/isDatabaseAvailable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-until docker exec -it ccr_db_1 pg_isready --host=localhost --port=5432 --username=res_user --dbname=example #&>/dev/null
-  do
-  	echo "Waiting for postgresql connection with user res_user and db ccr to be available..."
-		sleep 1
-	done
