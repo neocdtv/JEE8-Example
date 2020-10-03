05d16c75ea6f14818df77e3f900a631737ed937e
neocdtv <krzysztof.wolf@nttdata.com>
2020-10-03 16:21:05 +0200

    add new entites

--- database/src/main/resources/db/migration/V20200804__init.sql
+++ database/src/main/resources/db/migration/V20200804__init.sql
@@ -5,4 +5,21 @@ CREATE TABLE t_person (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE t_address (
+  id BIGINT NOT NULL,
+  city TEXT,
+  person_id BIGINT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (person_id) REFERENCES t_person(id)
+);
+
+CREATE TABLE t_organisation (
+  id BIGINT NOT NULL,
+  name TEXT,
+  PRIMARY KEY (id)
+);
+
 CREATE SEQUENCE sq_person START WITH 1;
+CREATE SEQUENCE sq_address START WITH 1;
+CREATE SEQUENCE sq_orgranisation START WITH 1;
+
