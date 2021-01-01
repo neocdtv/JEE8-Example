555f1839724f0f11d288be1cff84f0de72868c3f
neocdtv <krzysztof.wolf.1980@gmail.com>
2021-01-01 06:54:22 -0800

    check the outcome of a submited task, and "mark" es failed

--- app/src/main/java/io/neocdtv/jee8/app/control/ConcurrencyUtil.java
+++ app/src/main/java/io/neocdtv/jee8/app/control/ConcurrencyUtil.java
@@ -3,17 +3,28 @@ package io.neocdtv.jee8.app.control;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class ConcurrencyUtil {
+
+  private final static Logger LOGGER = Logger.getLogger(ConcurrencyUtil.class.getName());
 
   public boolean canAddTask(List<Future<?>> tasks, int maxSize) {
     Iterator<Future<?>> iterator = tasks.iterator();
     while (iterator.hasNext()) {
       Future<?> task = iterator.next();
       if (task.isDone()) {
-        iterator.remove();
+        try {
+          task.get();
+        } catch (InterruptedException | ExecutionException e) {
+          LOGGER.warning(e.getMessage());
+          return false;
+        } finally {
+          iterator.remove();
+        }
       }
     }
     if (tasks.size() < maxSize) {
